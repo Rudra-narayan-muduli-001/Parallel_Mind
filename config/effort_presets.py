39949fed54ed3c9e44103aeba597910f04bd3f13
Rudra Narayan Muduli <rudranarayanmuduli001@gmail.com>
2026-07-16 22:28:14 +0530

    Write effort pre sets

---
+++ config/effort_presets.py
@@ -0,0 +1,12 @@
+EFFORT_PRESETS = {
+    "low": {
+        "temperature": 0.3,
+        "max_tokens": 1024,
+        "timeout_sec": 30,
+    },
+    "high": {
+        "temperature": 0.7,
+        "max_tokens": 4096,
+        "timeout_sec": 90,
+    },
+}
