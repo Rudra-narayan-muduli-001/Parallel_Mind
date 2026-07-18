bf36f4230b7e3931c0722aa384a07489962d594c
Rudra Narayan Muduli <rudranarayanmuduli001@gmail.com>
2026-07-18 21:04:01 +0530

    Populate pipelines/research/__init__.py with key exports

---
+++ pipelines/research/__init__.py
@@ -0,0 +1,6 @@
+from pipelines.research.pipeline import ResearchPipeline
+from pipelines.research.planner import ResearchPlanner
+from pipelines.research.researcher_agent import ResearcherAgent
+from pipelines.research.aggregator import build_research_aggregator
+
+__all__ = ["ResearchPipeline", "ResearchPlanner", "ResearcherAgent", "build_research_aggregator"]
