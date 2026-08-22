8dfc374ca1d9fb2adb989d46d1da7708612388f2
Rudra Narayan Muduli <rudranarayanmuduli001@gmail.com>
2026-08-22 11:12:37 +0530

    Update research_cmd.py

--- cli/commands/research_cmd.py
+++ cli/commands/research_cmd.py
@@ -12,6 +12,7 @@ from pipelines.research.pipeline import ResearchPipeline
 
 
 async def run_research(topic: str):
+    providers = build_providers(settings)
     catalog = await ModelCatalog.from_providers(providers)
     run_config = run_wizard(catalog)
     providers = build_providers(settings)
@@ -20,7 +21,7 @@ async def run_research(topic: str):
         policy: RuleBasedPolicy | ManualPolicy = ManualPolicy(run_config.selected_targets, run_config.effort)
         gen_params = dict(EFFORT_PRESETS[run_config.effort])
     else:
-        policy = RuleBasedPolicy(providers=providers)
+        policy = RuleBasedPolicy(providers=providers, catalog=catalog)
         gen_params = {}
 
     router = Router(policy)
